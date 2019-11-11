02e24e849e4bfbec3dfeded85c63995302fcb286
tyopresnus <tyo.presnus@gmail.com>
2019-11-11 15:35:32 +0700

    latihan stagehand console-full

---
+++ Novice/01-01/Kasus/hallo2/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:hallo2/hallo2.dart' as hallo2;
+
+main(List<String> arguments) {
+  print('Hello world: ${hallo2.calculate()}!');
+}
---
+++ Novice/01-01/Kasus/hello/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:hello/hello.dart' as hello;
+
+main(List<String> arguments) {
+  print('Hello world: ${hello.calculate()}!');
+}
